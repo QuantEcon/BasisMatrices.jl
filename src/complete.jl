ea33dbe2f099ff0f8959580944afefdcadabf44d
Chase Coleman <cc7768@gmail.com>
2017-02-07 17:11:06 -0500

    Add complete poly at one point and complete poly derivs
    * Change order and add vector methods
    
    * Add tests and condense code.
    
    * Add derivative function for vectors
    
    * Add matrix derivatives -- Needs speedup
    
    * Add tests for derivatives.
    
    * Remove _der in complete_polynomial_der names...
    
    * Change order in docs.
    
    * fix returns in comlpete polynomial code
    
    * add ifelse instead of ternary
    
    * Small cleanups and add todos
    
    * Fix tests for complete :doh:

--- src/complete.jl
+++ src/complete.jl
@@ -12,15 +12,23 @@ should be `z = [q r s]`
 
 Output is a basis matrix. In our example, with `d` set to 2 we would have
 
-TODO: update docstring to properly give order of terms
-
 ```julia
-out = [ones(size(z,1)) q r s q.*r q.*s r.*s q.^2 q.*r q.*s r.^2 r.*s s.^2]
+out = [ones(size(z,1)) q q.^2 q.*r q.*s r r.^2 r.*s s s.^2]
 ```
+
+TODO:
+  * Currently a bit more code repetition than is desireable. It would be
+    nice to cut down on the repetition between the `complete_polynomial`
+    functions
+  * Mutating arguments are currently last... Change them to first
+  * Current algorithm for computing derivatives is kind of slow -- Is
+    there any obvious ways to improve this?
+
 """
 :complete_polynomial
 
 immutable Degree{N} end
+immutable Derivative{D} end
 
 function n_complete(n::Int, D::Int)
     out = 1
@@ -34,11 +42,63 @@ function n_complete(n::Int, D::Int)
     out
 end
 
-@generated function complete_polynomial!{N}(z::Matrix, d::Degree{N},
-                                            out::Matrix)
+#
+# Generating basis functions
+#
+@generated function complete_polynomial!{N,T,Ndim}(z::Array{T,Ndim}, d::Degree{N},
+                                                   out::Array{T,Ndim})
     complete_polynomial_impl!(z, d, out)
 end
 
+function complete_polynomial!{T, Ndim}(z::Array{T,Ndim}, d::Int, out::Array{T,Ndim})
+    complete_polynomial!(z, Degree{d}(), out)
+
+    return out
+end
+
+
+#
+# Vector versions for generating basis functions
+#
+function complete_polynomial_impl!{T,N}(z::Type{Vector{T}}, ::Type{Degree{N}},
+                                        out::Type{Vector{T}})
+    outer_temp = Expr(:(=), Symbol("tmp_$(N+1)"), one(T))
+    quote
+        nvar = length(z)
+        if length(out) != (n_complete(nvar, $N))
+            error("z, out not compatible")
+        end
+
+        # reset first column to ones
+        out[1] = one($T)
+
+        ix = 1
+        $outer_temp
+        @nloops($N, # number of loops
+                i,  # counter
+                d->((d == $N ? 1 : i_{d+1}) : nvar),  # ranges
+                d->((begin
+                        ix += 1
+                        tmp_d = tmp_{d+1}*z[i_d]
+                        out[ix] = tmp_d
+                    end)),  # preexpr
+                Expr(:block, :nothing)  # bodyexpr
+                )
+        out
+    end
+end
+
+function complete_polynomial{T}(z::Vector{T}, d::Int)
+    nvar = length(z)
+    out = Array(T, n_complete(nvar, d))
+    complete_polynomial!(z, Degree{d}(), out)
+
+    return out
+end
+
+#
+# Matrix versions for generating basis functions
+#
 function complete_polynomial_impl!{T,N}(z::Type{Matrix{T}}, ::Type{Degree{N}},
                                         out::Type{Matrix{T}})
     quote
@@ -52,17 +112,11 @@ function complete_polynomial_impl!{T,N}(z::Type{Matrix{T}}, ::Type{Degree{N}},
             out[i, 1] = one($T)
         end
 
-        # set next nvar columns to input matrix
-        @inbounds for n=2:nvar+1, i=1:nobs
-            out[i, n] = z[i, n-1]
-        end
-
-        ix = nvar+1
+        ix = 1
         @nloops($N, # number of loops
                 i,  # counter
                 d->((d == $N ? 1 : i_{d+1}) : nvar),  # ranges
-                d->(d == $N ? nothing :
-                    (begin
+                d->((begin
                         ix += 1
                         @inbounds @simd for r=1:nobs
                             tmp = one($T)
@@ -79,9 +133,119 @@ end
 function complete_polynomial{T}(z::Matrix{T}, d::Int)
     nobs, nvar = size(z)
     out = Array(T, nobs, n_complete(nvar, d))
-    complete_polynomial!(z, Degree{d}(), out)::Matrix{T}
+    complete_polynomial!(z, Degree{d}(), out)
+
+    return out
 end
 
-function complete_polynomial!{T}(z::Matrix{T}, d::Int, out::Matrix{T})
-    complete_polynomial!(z, Degree{d}(), out)::Matrix{T}
+#
+# Generating 1st derivative of basis functions
+#
+@generated function complete_polynomial!{N,D,T,Ndim}(z::Array{T,Ndim}, d::Degree{N},
+                                                     der::Derivative{D}, out::Array{T,Ndim})
+    complete_polynomial_impl!(z, d, der, out)
 end
+
+function complete_polynomial!{T, Ndim}(z::Array{T,Ndim}, d::Int, der::Int, out::Array{T,Ndim})
+    complete_polynomial!(z, Degree{d}(), Derivative{der}(), out)::Array{T,Ndim}
+
+    return out
+end
+
+#
+# Vector versions for generating first derivative of basis functions
+#
+function complete_polynomial_impl!{T,N,D}(z::Type{Vector{T}}, ::Type{Degree{N}},
+                                          ::Type{Derivative{D}}, out::Type{Vector{T}})
+    notD_top = Expr(:(=), Symbol("notD_$(N+1)"), one(T))
+    coeff_top = Expr(:(=), Symbol("coeff_$(N+1)"), zero(T))
+    quote
+        nvar = length(z)
+        if length(out) != (n_complete(nvar, $N))
+            error("z, out not compatible")
+        end
+
+        # reset first element to zero
+        out[1] = zero($T)
+
+        ix = 1
+        $notD_top
+        $coeff_top
+        @nloops($N, # number of loops
+                i,  # counter
+                d->((d == $N ? 1 : i_{d+1}) : nvar),  # ranges
+                d->((begin
+                        ix += 1
+                        # Depending on what i_d is, update variables
+                        if i_d == D
+                            coeff_d = coeff_{d+1} + 1
+                            notD_d = notD_{d+1}
+                        else
+                            coeff_d = coeff_{d+1}
+                            notD_d = notD_{d+1}*z[i_d]
+                        end
+
+                        out[ix] = coeff_d * z[D]^(coeff_d-1) * notD_d
+                    end)),  # preexpr
+                Expr(:block, :nothing)  # bodyexpr
+                )
+        out
+    end
+end
+
+function complete_polynomial{T}(z::Vector{T}, d::Int, der::Int)
+    nvar = length(z)
+    out = Array(T, n_complete(nvar, d))
+    complete_polynomial!(z, Degree{d}(), Derivative{der}(), out)::Vector{T}
+
+    return out
+end
+
+#
+# Matrix versions for generating first derivative of basis functions
+#
+function complete_polynomial_impl!{T,N,D}(z::Type{Matrix{T}}, ::Type{Degree{N}},
+                                          ::Type{Derivative{D}}, out::Type{Matrix{T}})
+    coeff_top = Expr(:(=), Symbol("coeff_$(N+1)"), zero(T))
+    quote
+        nobs, nvar = size(z)
+        if size(out) != (nobs, n_complete(nvar, $N))
+            error("z, out not compatible")
+        end
+
+        # reset first element to zero
+        @inbounds @simd for r=1:nobs
+            out[r, 1] = zero($T)
+        end
+
+        ix = 1
+        $coeff_top
+        @nloops($N, # number of loops
+                i,  # counter
+                d->((d == $N ? 1 : i_{d+1}) : nvar),  # ranges
+                d->((begin
+                        ix += 1
+
+                        # Depending on what i_d is, update variables
+                        coeff_d = ifelse(i_d == D, coeff_{d+1} + 1, coeff_{d+1})
+
+                        @inbounds @simd for r=1:nobs
+                            tmp = one($T)
+                            @nexprs $N-d+1 j->(tmp *= ifelse(i_{$N-j+1} != D, z[r, i_{$N-j+1}], one($T)))
+                            out[r, ix] = coeff_d * tmp * z[r, D]^(coeff_d-1)
+                        end
+                    end)),  # preexpr
+                Expr(:block, :nothing)  # bodyexpr
+                )
+        out
+    end
+end
+
+function complete_polynomial{T}(z::Matrix{T}, d::Int, der::Int)
+    nobs, nvar = size(z)
+    out = Array(T, nobs, n_complete(nvar, d))
+    complete_polynomial!(z, Degree{d}(), Derivative{der}(), out)::Matrix{T}
+
+    return out
+end
+
--- test/complete.jl
+++ test/complete.jl
@@ -1,36 +1,85 @@
 @testset "Testing logic using strings" begin
     Base.one{T<:AbstractString}(::Type{T}) = "one "
-    z = ["x " "y " "z "]
+    z = ["x ", "y ", "z "]
 
-    want_1 = ["one " "x " "y " "z "]
-    @test complete_polynomial(z, 1) == want_1
+    want_1 = ["one " "one x " "one y " "one z "]
+    @test complete_polynomial(reshape(z, 1, length(z)), 1) == want_1
+    # Test vector methods to make sure generates same thing as matrix methods
+    @test complete_polynomial(z, 1) == want_1[:]
 
-    want_2 = ["one " "x " "y " "z " "one x x " "one x y " "one x z " "one y y " "one y z " "one z z "]
-    @test complete_polynomial(z, 2) == want_2
+    want_2 = ["one " "one x " "one x x " "one x y " "one x z " "one y " "one y y " "one y z " "one z " "one z z "]
+    @test complete_polynomial(reshape(z, 1, length(z)), 2) == want_2
+    # Test vector methods to make sure generates same thing as matrix methods
+    @test complete_polynomial(z, 2) == want_2[:]
+
 end
 
 @testset "testing numerical routintes" begin
     z = rand(100, 10)
     the_ones = ones(size(z, 1))
+    the_zeros = zeros(size(z, 1))
     for d in 1:5
         n_comp = n_complete(size(z, 2), d)
+
+        # Test basis matrices
         buff_d = Array(Float64, size(z, 1), n_comp)
         out_d = complete_polynomial(z, d)
         complete_polynomial!(z, d, buff_d)
         @test size(out_d, 1) == size(z, 1)
         @test size(out_d, 2) == n_comp
-        @test out_d[:, 1] == the_ones
-        @test out_d[:, 2:size(z, 2)+1] == z
-        @test out_d == buff_d
+        @test all(isapprox.(out_d[:, 1], the_ones))
+        @test all(isapprox.(out_d[:, 2], z[:, 1]))
+        @test d == 1 ? true : all(isapprox.(out_d[:, 3], z[:, 1].*z[:, 1]))
+        @test all(isapprox.(out_d[:, end], z[:, end].^d))
+        @test all(isapprox.(out_d, buff_d))
+
+        # Test derivatives
+        buff_der_d = Array(Float64, size(z, 1), n_comp)
+        out_der_d = complete_polynomial(z, d, 1)
+        complete_polynomial!(z, d, 1, buff_der_d)
+        @test size(out_der_d, 1) == size(z, 1)
+        @test size(out_der_d, 2) == n_comp
+        @test all(isapprox.(out_der_d[:, 1], the_zeros))
+        @test all(isapprox.(out_der_d[:, 2], the_ones))
+        @test d ==1 ? true : all(isapprox.(out_der_d[:, 3], 2.*z[:, 1]))
+        @test d <= 2 ? true : all(isapprox.(out_der_d[:, 4], 3.*z[:, 1].*z[:, 1]))
+        @test all(isapprox.(out_der_d[:, end], the_zeros))
+        @test all(isapprox.(out_der_d, buff_der_d))
+
     end
 
     z = rand(10, 3)
+    z2 = z[1, :]
+    all_zero = zeros(10)
+    all_ones = ones(10)
     out_2 = complete_polynomial(z, 2)
     # need to test columns size(z, 2) + 2:end
-    @test out_2[:, 5] == z[:, 1].^2
-    @test out_2[:, 6] == z[:, 1].*z[:, 2]
-    @test out_2[:, 7] == z[:, 1].*z[:, 3]
-    @test out_2[:, 8] == z[:, 2].*z[:, 2]
-    @test out_2[:, 9] == z[:, 2].*z[:, 3]
-    @test out_2[:, 10] == z[:, 3].*z[:, 3]
+    @test all(isapprox.(out_2[:, 1], all_ones))
+    @test all(isapprox.(out_2[:, 2], z[:, 1]))
+    @test all(isapprox.(out_2[:, 3], z[:, 1].*z[:, 1]))
+    @test all(isapprox.(out_2[:, 4], z[:, 1].*z[:, 2]))
+    @test all(isapprox.(out_2[:, 5], z[:, 1].*z[:, 3]))
+    @test all(isapprox.(out_2[:, 6], z[:, 2]))
+    @test all(isapprox.(out_2[:, 7], z[:, 2].*z[:, 2]))
+    @test all(isapprox.(out_2[:, 8], z[:, 2].*z[:, 3]))
+    @test all(isapprox.(out_2[:, 9], z[:, 3]))
+    @test all(isapprox.(out_2[:, 10], z[:, 3].*z[:, 3]))
+    # Test vector methods to make sure generates same thing as matrix methods
+    @test all(isapprox.(out_2[1, :], complete_polynomial(z2, 2)))
+
+    out_der_2 = complete_polynomial(z, 2, 2)
+    # need to test columns size(z, 2) + 2:end
+    @test all(isapprox.(out_der_2[:, 1], all_zero))
+    @test all(isapprox.(out_der_2[:, 2], all_zero))
+    @test all(isapprox.(out_der_2[:, 3], all_zero))
+    @test all(isapprox.(out_der_2[:, 4], z[:, 1]))
+    @test all(isapprox.(out_der_2[:, 5], all_zero))
+    @test all(isapprox.(out_der_2[:, 6], all_ones))
+    @test all(isapprox.(out_der_2[:, 7], 2 .* z[:, 2]))
+    @test all(isapprox.(out_der_2[:, 8], z[:, 3]))
+    @test all(isapprox.(out_der_2[:, 9], all_zero))
+    @test all(isapprox.(out_der_2[:, 10], all_zero))
+    # Test vector methods to make sure generates same thing as matrix methods
+    @test all(isapprox.(out_2[1, :], complete_polynomial(z2, 2)))
+
 end
